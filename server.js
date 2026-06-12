73fb54d372007ae678b9e1f50d8994a1624d193d
rochmadnf <rochmadnf@gmail.com>
2026-06-12 11:23:20 +0800

    fix error print

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import os from "node:os";
-import { spawn } from "node:child_process";
 
 const services = [];
 
@@ -17,57 +16,7 @@ function getLocalIp() {
   return "127.0.0.1";
 }
 
-function draw() {
-  console.clear();
-
-  const ip = getLocalIp();
-
-  console.log("========================================");
-  console.log(`IP Address : ${ip}`);
-  console.log("========================================");
-  console.log("");
-
-  services.forEach((service) => {
-    const icon = service.running ? "🟢" : "🔴";
-
-    console.log(`${icon} ${service.name.padEnd(10)} ${service.url}`);
-  });
-
-  console.log("");
-  console.log("Ctrl+C untuk menghentikan semua service");
-}
-
-function startService(name, command, args, url) {
-  const service = {
-    name,
-    url,
-    running: true,
-  };
-
-  services.push(service);
-
-  const child = spawn(command, args, {
-    shell: true,
-    stdio: "ignore",
-  });
-
-  child.on("exit", () => {
-    service.running = false;
-    draw();
-  });
-
-  draw();
-
-  return child;
-}
-
 const ip = getLocalIp();
 
-startService(
-  "PHP",
-  "php",
-  ["-S", "0.0.0.0:8082", "-t", "./print-struck"],
-  `http://${ip}:8082`,
-);
-
-startService("NODE", "serve", ["-l", "3000", ""], `http://${ip}:3000`);
+console.log("========================================");
+console.log(`🛜 IP: ${ip}`);
